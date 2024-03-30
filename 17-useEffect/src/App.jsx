32c91142bac8cc2b21af1d893627b0c249fdeb87
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-03-30 23:54:25 +0500

    useEffect end

--- 17-useEffect/src/App.jsx
+++ 17-useEffect/src/App.jsx
@@ -13,20 +13,30 @@ import { useEffect } from 'react';
 //     return <div className="App"></div>;
 // }
 
+// function App() {
+//     const [todo, setTodo] = useState(null);
+//     useEffect(() => {
+//         fetch('https://jsonplaceholder.typicode.com/todos/13')
+//         .then((response) => response.json())
+//         .then((json) => setTodo(json));
+//     }, [])
+//     console.log('App rendered');
+//     console.log(todo);
+
+//     return <div className="App"></div>;
+// }
+
 function App() {
     const [todo, setTodo] = useState(null);
     useEffect(() => {
         fetch('https://jsonplaceholder.typicode.com/todos/13')
-        .then((response) => response.json())
-        .then((json) => setTodo(json));
-    }, [])
+            .then((response) => response.json())
+            .then((json) => setTodo(json));
+    }, []);
     console.log('App rendered');
     console.log(todo);
 
-    
-
-    return <div className="App"></div>;
+    return <div className="App">{todo && <h1>{todo.title}</h1>}</div>;
 }
-
 
 export default App;
