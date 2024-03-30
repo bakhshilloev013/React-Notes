4d5a62d5ec0b8482b7ff5e71bfb7830dfd2c4a4b
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-03-30 23:39:48 +0500

    useEffect

--- 17-useEffect/src/App.jsx
+++ 17-useEffect/src/App.jsx
@@ -1,11 +1,32 @@
+import { useState } from 'react';
 import './App.css';
+import { useEffect } from 'react';
+
+// function App() {
+//     const [todo, setTodo] = useState(null);
+//     console.log('App rendered');
+
+//     fetch('https://jsonplaceholder.typicode.com/todos/13')
+//         .then((response) => response.json())
+//         .then((json) => setTodo(json));
+
+//     return <div className="App"></div>;
+// }
 
 function App() {
-    fetch('https://jsonplaceholder.typicode.com/todos')
+    const [todo, setTodo] = useState(null);
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos/13')
         .then((response) => response.json())
-        .then((json) => console.log(json));
+        .then((json) => setTodo(json));
+    }, [])
+    console.log('App rendered');
+    console.log(todo);
+
+    
 
     return <div className="App"></div>;
 }
+
 
 export default App;
