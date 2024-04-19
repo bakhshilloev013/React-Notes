452f48516181204ce412bb9d9ab8b1b76b940700
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-04-20 01:03:27 +0500

    todos actions start

---
+++ 22.3-todo-app-v2/src/components/Todos/TodosActions.jsx
@@ -0,0 +1,10 @@
+import { TfiReload } from 'react-icons/tfi';
+import { RiDeleteBin2Line } from 'react-icons/ri';
+import Button from '../UI/Button';
+import styles from './TodosActions.module.css';
+
+function TodosActions() {
+    return <div></div>;
+}
+
+export default TodosActions;
