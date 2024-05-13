be4b9dc26101027d4331f746e634f3b2e6acd0fb
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-05-13 21:27:58 +0500

    location and query string task complete

--- 24.4-react-courses-useLocation/src/components/Courses.jsx
+++ 24.4-react-courses-useLocation/src/components/Courses.jsx
@@ -1,33 +1,47 @@
+import { useEffect, useState } from 'react';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
 import queryString from 'query-string'; // для этого нужно установить пакет query-string
 import courses from '../data/courses';
 
 function Courses() {
+    const [data, setData] = useState([...courses]);
     const navigate = useNavigate();
     const location = useLocation();
     const parsed = queryString.parse(location.search); // когда мы получаем "объект" из метода parse на самом деле это не объект
     // По этой причине мы строем новый объект из полученной строки
     const params = Object.assign({}, parsed);
     const sortValue = Object.values(params)[0];
-    if (location.search) {
+    let titleText = 'Courses';
+    useEffect(() => {
         if (params.hasOwnProperty('sort')) {
-            courses.forEach((item) => {
-                if (item.hasOwnProperty(sortValue)) {
-                    console.log(item);
-                } else {
-                    navigate('/courses', { relative: 'route' });
-                }
-            });
+            if (data.every((item) => item.hasOwnProperty(sortValue))) {
+                data.sort((a, b) => {
+                    if (a[sortValue] > b[sortValue]) {
+                        return -1;
+                    } else if (a[sortValue] < b[sortValue]) {
+                        return 1;
+                    } else {
+                        return 0;
+                    }
+                });
+                setData([...data]);
+            } else {
+                navigate('/courses', { relative: 'route' });
+            }
         } else {
             navigate('/courses', { relative: 'route' });
         }
-    }
+    }, [location.search, navigate]);
 
     return (
         <>
-            <h1>Courses</h1>
+            {location.search ? (
+                <h1>Courses sorted by {sortValue}</h1>
+            ) : (
+                <h1>Courses</h1>
+            )}
             <div>
-                {courses.map((item) => (
+                {data.map((item) => (
                     <Link key={item.id} className="courseLink" to={item.slug}>
                         {item.title}
                     </Link>
