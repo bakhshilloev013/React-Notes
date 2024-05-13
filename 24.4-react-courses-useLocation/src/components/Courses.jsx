1902248c4faeaf375d0d74f795cb584a640280ec
Bekhzod Bakhshilloev <beki2627@gmail.com>
2024-05-13 11:17:54 +0500

    location and query string

--- 24.4-react-courses-useLocation/src/components/Courses.jsx
+++ 24.4-react-courses-useLocation/src/components/Courses.jsx
@@ -1,13 +1,34 @@
-import { Link} from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import queryString from 'query-string'; // для этого нужно установить пакет query-string
 import courses from '../data/courses';
 
 function Courses() {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const parsed = queryString.parse(location.search); // когда мы получаем "объект" из метода parse на самом деле это не объект
+    // По этой причине мы строем новый объект из полученной строки
+    const params = Object.assign({}, parsed);
+    const sortValue = Object.values(params)[0];
+    if (location.search) {
+        if (params.hasOwnProperty('sort')) {
+            courses.forEach((item) => {
+                if (item.hasOwnProperty(sortValue)) {
+                    console.log(item);
+                } else {
+                    navigate('/courses', { relative: 'route' });
+                }
+            });
+        } else {
+            navigate('/courses', { relative: 'route' });
+        }
+    }
+
     return (
         <>
             <h1>Courses</h1>
             <div>
-                {courses.map((item, ) => (
-                    <Link key={item.id} className='courseLink' to={item.slug}>
+                {courses.map((item) => (
+                    <Link key={item.id} className="courseLink" to={item.slug}>
                         {item.title}
                     </Link>
                 ))}
